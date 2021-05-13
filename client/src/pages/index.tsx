afdfa6d16e3fc6d5a22f6dbe1f66b7132650d2f3
Lucas dev <devlucasmarques@gmail.com>
2021-05-13 15:58:03 -0300

    create setup

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Pagina</title>
+      </Head>
+
+      <main></main>
+    </div>
+  );
+};
+
+export default Home;
